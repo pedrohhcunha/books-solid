b2d1abfbafe2631b4d3d14d03dd156515a056f33
Pedro Cunha <pedro.cunha@onfinity.com.br>
2022-10-12 00:05:35 -0300

    feat(book-entity): validate if title, author and genre are length valid

--- entities/book.ts
+++ entities/book.ts
@@ -20,6 +20,20 @@ export class Book {
     }
 
     constructor(props: BookProps) {
+        const { title, author, genre } = props;
+
+        if(title.length === 0 || title.length > 144) {
+            throw new Error('The title must be between 1 and 144 characters');
+        }
+
+        if(author.length === 0 || author.length > 44) {
+            throw new Error('The author must be between 1 and 44 characters');
+        }
+
+        if(genre.length === 0 || genre.length > 16) {
+            throw new Error('The genre must be between 1 and 16 characters');
+        }
+
         this.props = props;
     }
 }
