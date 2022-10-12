e87645d12cbc2886c1b868e0be290c40f21482ae
Pedro Cunha <pedro.cunha@onfinity.com.br>
2022-10-12 17:15:03 -0300

    feat(book-controller): create logic to create book method

--- src/controllers/books.controller.ts
+++ src/controllers/books.controller.ts
@@ -1,9 +1,34 @@
 import { Request, Response } from "express";
+import { InMemoryBooksRepository } from "../repositories/inMemory/inMemoryBooksRepository";
+import { CreateBook } from "../services/createBook";
 
 export class BooksController {
     public async createBook(req: Request, res: Response) {
-        res.status(200).json({
-            message: 'POST BOOKS',
+        const { title, author, genre } = req.body;
+
+        if (!title || !author || !genre) {
+            return res.status(400).json({
+                message: 'Bad Request',
+            });
+        }
+
+        const booksRepository = new InMemoryBooksRepository();
+        const createBook = new CreateBook(booksRepository);
+
+        const book = await createBook.execute({
+            title,
+            author,
+            genre,
+        });
+
+        res.status(201).json({
+            message: 'Book created successfully',
+            book: {
+                id: book.id,
+                title: book.title,
+                author: book.author,
+                genre: book.genre,
+            },
         });
     }
 
