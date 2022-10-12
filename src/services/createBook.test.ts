2bc4cfbaa03cde6ed57b4557fdd9f3c361ff7211
Pedro Cunha <pedro.cunha@onfinity.com.br>
2022-10-12 01:10:43 -0300

    test(book-service): write test to check with book validates work correctly

--- src/services/createBook.test.ts
+++ src/services/createBook.test.ts
@@ -1,10 +1,12 @@
 import { describe, it, expect } from "vitest";
 import { CreateBook } from "./createBook";
 import { Book } from "../entities/book";
+import { InMemoryBooksRepository } from "../repositories/inMemory/inMemoryBooksRepository";
 
 describe("createBook - Services", () => {
     it('should create a book', () => {
-        const sut = new CreateBook();
+        const booksRepository = new InMemoryBooksRepository();
+        const sut = new CreateBook(booksRepository);
 
         expect(sut.execute({
             title: 'The Lord of the Rings',
@@ -12,5 +14,22 @@ describe("createBook - Services", () => {
             genre: 'Fantasy',
         })).resolves.toBeInstanceOf(Book);
 
+    });
+
+    it('should not create a book if it already exists', async () => {
+        const booksRepository = new InMemoryBooksRepository();
+        const sut = new CreateBook(booksRepository);
+
+        await sut.execute({
+            title: 'The Lord of the Rings',
+            author: 'J.R.R. Tolkien',
+            genre: 'Fantasy',
+        });
+
+        expect(sut.execute({
+            title: 'The Lord of the Rings',
+            author: 'J.R.R. Tolkien',
+            genre: 'Fantasy',
+        })).rejects.toThrowError('Book already exists');
     });
 });
