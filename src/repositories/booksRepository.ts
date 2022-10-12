6416d94d8e1db70943954cb320cf7c2e76ca9269
Pedro Cunha <pedro.cunha@onfinity.com.br>
2022-10-12 01:08:33 -0300

    test(book-service): Create in memory reporitory to test service layer

---
+++ src/repositories/booksRepository.ts
@@ -0,0 +1,6 @@
+import { Book } from "../entities/book";
+
+export interface BooksRepository {
+    create(book: Book): Promise<void>;
+    findByTitle(title: string): Promise<Book | undefined>;
+}
---
+++ src/repositories/inMemory/inMemoryBooksRepository.ts
@@ -0,0 +1,14 @@
+import { BooksRepository } from "../booksRepository";
+import { Book } from "../../entities/book";
+
+export class InMemoryBooksRepository implements BooksRepository {
+    private books: Book[] = [];
+
+    async create(book: Book): Promise<void> {
+        this.books.push(book);
+    }
+
+    async findByTitle(title: string): Promise<Book | undefined> {
+        return this.books.find(book => book.title === title);
+    }
+}
