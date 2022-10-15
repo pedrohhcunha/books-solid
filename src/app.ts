8ebd89a596eded3b656648ae8c3dd36f33ab2d86
Pedro Cunha <pedro.cunha@onfinity.com.br>
2022-10-15 00:08:04 -0300

    fix(cors): allow front to access cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { indexRouter } from "./routes/index.routes";
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 app.use(indexRouter);
 
